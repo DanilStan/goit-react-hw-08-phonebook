397a35cc450f39f79f7ebc078f1d15f41f3f43e6
DanilStan <stanislavov.danik@gmail.com>
2022-11-10 18:13:38 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Router, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { fetchCurrentUser } from 'redux/auth/auth-operations';
+import { getToken } from 'redux/selectors';
+
+import { Header } from './Header';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
+const Home = lazy(() => import('../pages/Home'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
+const Register = lazy(() => import('../pages/Register'));
+const Login = lazy(() => import('../pages/Login'));
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const token = useSelector(getToken);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchCurrentUser());
+    }
+    return;
+  }, [dispatch, token]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Header />
+      <main>
+        <Suspense>
+          <Router basename="/goit-react-hw-08-phonebook">
+            <Routes>
+              <Route path="/" element={<Home />} />
+
+              <Route path="/" element={<PrivateRoute />}>
+                <Route path="/contacts" element={<ContactsPage />} />
+              </Route>
+
+              <Route path="/" element={<PublicRoute />}>
+                <Route path="/registration" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+              </Route>
+            </Routes>
+          </Router>
+        </Suspense>
+      </main>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 };
